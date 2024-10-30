372c8faf55537474681da38df5963a80280a38f0
dayne-fisher <daynefisher13@gmail.com>
2024-10-30 08:54:34 -0500

    sixth update

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 import pandas as pd
 import streamlit as st
-st.write("hello")
-st.header("Vehicle Data in the US")
+import plotly.express as px
+import numpy as np
+
+st.header("Market of Vehicles in the US")
+st.write("Filter the data below to see the by each manufacturer")
+
+df = pd.read_csv('vehicles_us.csv')
+df = df.drop(df.columns[0], axis=1)
+
+manufacturer_choice = df['manufacturer_name'].unique()
+
+st.selectbox('Select a Manufacturer')
+
