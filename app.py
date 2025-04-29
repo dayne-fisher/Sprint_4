b0be6d76e8c031d2301a9b6108407c3bc08a12d3
dayne-fisher <daynefisher13@gmail.com>
2025-04-28 19:32:59 -0500

    update

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ import streamlit as st
 import plotly.express as px
 import numpy as np
 
-st.header("Ads of Vehicles in the US")
-st.write("Filter the data below to see the by each manufacturer")
-
 
 df = pd.read_csv('vehicles_us.csv')
 df = df.drop(df.columns[0], axis=1)
