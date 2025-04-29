02de4568fe7afd0863a022fc79e954e9289bb90b
dayne-fisher <daynefisher13@gmail.com>
2025-04-28 19:24:05 -0500

    update

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
+# Title: What affects the number of days a car is listed on the market?
+# In this project, we will look at data about vehicles in the US and see the number of days by each vehicle they are listed and analyze what variables affect the amount the most. We can then presnt this information and use it to help which vehicles should be focused on more for advertising to sell.
+
+# To complete this project, you will have to import the standard packages to start to run the data. 
+# Those would be pandas, numpy, plotly.express and streamlit. 
+# After running this code, we need to read the data set itself. The data is about the Ads by each vehicle in the US. 
+# Once the data has been uploaded, we create a selectbox to show data filtered by each model in the data.
+# We then run a code to create a histogram and scatterplot to show how varialbes affect the number of days a vehicle is posted for.
+# After creating these graphs, we can analyze which variables affect the data the most and come up with a solution to help lower the days a vehicles is posted and get it to sell.
+
+
+
 https://sprint-4-ftk5.onrender.com/
 
--- app.py
+++ app.py
@@ -4,27 +4,6 @@ import streamlit as st
 import plotly.express as px
 import numpy as np
 
-st.header("Ads of Vehicles in the US")
-st.write("Filter the data below to see the by each manufacturer")
-
-df = pd.read_csv('vehicles_us.csv')
-df = df.drop(df.columns[0], axis=1)
-
-model_choice = df['model'].unique()
-
-selected_model = st.selectbox('Select a Model', model_choice)
-
-min_days_listed, max_days_listed = int(df['days_listed'].min()), int(df['days_listed'].max())
-
-days_listed_range = st.slider("Select Number of Days Listed", value=(min_days_listed, max_days_listed), min_value=min_days_listed, max_value=max_days_listed)
-days_listed_range[0]
-days_listed_range[1]
-
-actual_range = list(range(days_listed_range[0], days_listed_range[1]+1))
-
-
-df_filtered = df[ (df.model == selected_model) & df.model_year.isin(list(actual_range))]
-
 
 st.header("Days Posted Analysis")
 st.write(""" Let's analyze what variable influences days posted the most between Condition or Model Year""")
@@ -37,7 +16,7 @@ fig1 = px.histogram(df, x="days_listed", color= selected_var)
 fig1.update_layout(title = "<b> Split of Days Posted by {}</b>".format(selected_var))
 st.plotly_chart(fig1)
 
-st.write(""" After viewing the data, we can tell that the condition influences the number of days listed. """)
+st.write(""" After viewing the data, we can tell that the condition influences the number of days listed more than the year the vehicle was made. """)
 
 def age_category(x):
     if x<5: return '<5'
@@ -55,7 +34,7 @@ fig2 = px.scatter(df, x='days_listed', y= choice_for_scatter, color = "age_categ
 fig2.update_layout(title = "<b> Days Listed vs {}</b>".format(choice_for_scatter))
 st.plotly_chart(fig2)
 
-st.write("""After viewing the data, we can tell that that the odometer number influences the number of days listed. """)
+st.write("""After viewing the data, we can tell that that the whether or not a vehicle is 4-wheel drive, influences the number of days listed the least. This could be becuase of many different reasons, but we do not have that data. This could be a good follow up to learn more. """)
 
 
 st.write (""" Overall, even though other factors come into play about how long a vehicle is listed, the condition has the greastest impact of the amount of days listed for vehicles in the US.  """)
